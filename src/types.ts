f8b163f7ba3b3e0007bf53de13388b6acf965e06
FreGitHu <101549035+FreGitHu@users.noreply.github.com>
2024-02-24 15:28:30 +0000

    Add P Tag

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,13 @@ export interface Title {
     _text: string
 }
 
+export interface PTag {
+    _text: string
+}
+
 export interface HTMLPlusPlus {
     title?: Title
+    p?: PTag
 }
 
 export interface Root {
